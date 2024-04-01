c909b1776cc5226bbd8c9fd218b36e8a93329086
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-04-01 16:54:15 +0100

    fix trailing whitespace error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 '''
-A function that that returns a list of 
-lists of integers representing the Pascal’s 
+A function that that returns a list of
+lists of integers representing the Pascal’s
 triangle of n:
 '''
 
