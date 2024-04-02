2e000b230840cb58b5833940e4c4c48414b94f52
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-04-02 01:26:11 +0100

    add comment

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,16 +11,22 @@ def pascal_triangle(n):
     '''
     pascal triangle algorithm
     '''
+    # Initialize an empty list
     triangle = []
     if n <= 0:
+        # Return an empty list if n is non-positive val.
         return []
 
     for i in range(n):
+        # First element of each row is always 1
         row = [1]
         if triangle:
             prev_row = triangle[-1]
+            # Calculate each element in the current row (except the first and last)
+            # by summing the corresponding elements from the previous row.
             for j in range(len(prev_row) - 1):
                 row.append(prev_row[j] + prev_row[j + 1])
+                # Last element of each row is always 1
             row.append(1)
         triangle.append(row)
 
