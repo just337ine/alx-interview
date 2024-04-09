fc63523666f7a572b4277d1dbbc95daa1a81cca0
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-04-09 18:42:59 +0100

    minimum op function

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    A method that calculates the fewest
+    number of operations needed to result in 
+    exactly n H characters in the file
+"""
+
+
+def minOperations(n):
+    """
+        prototype: def minOperations(n)
+        Return an integer
+        if n is impossible to achieve, return 0
+    """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
